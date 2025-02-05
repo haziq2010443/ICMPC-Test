fe4946a28220cc500f5d191c77a2a95baf655dae
Haziq <haziqiskandar443@gmail.com>
2025-02-05 15:50:15 +0800

    Initial commit - setup ASP.NET Core MVC Project. Added Product and User model

---
+++ Models/Product.cs
@@ -0,0 +1,25 @@
+﻿namespace ICMPC_Test.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Product
+    {
+        [Key]
+        public int productId { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string productName { get; set; }
+
+        public string description { get; set; }
+
+        public decimal price { get; set; }
+
+        //foreign key
+        public int userId { get; set; }
+
+        [ForeignKey("userId")]
+        public User user { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,25 @@
+﻿namespace ICMPC_Test.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class User
+    {
+        [Key]
+        public int userId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string userName { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string email { get; set; }
+
+        [Required]
+        public string passwordHash { get; set; } //Store hashed password for security
+
+        //Navigate to products
+        public ICollection<Product> Products { get; set; }
+    }
+}
